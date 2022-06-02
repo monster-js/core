589303072f3f696d28087f70e9d205ca656c757a
darius <dariusa.bualanjr@pccw.com>
2022-06-02 09:46:30 +0800

    added generic type for fn-component

--- src/component/utils/fn-component.ts
+++ src/component/utils/fn-component.ts
@@ -9,20 +9,20 @@ import { Pipes } from "../../pipes/pipes.decorator";
 import { Services } from "../../service/services.decorator";
 import { Component } from "../component.decorator";
 
-interface HooksComponentConfigInterface {
-    inject?: ObjectInterface;
+interface HooksComponentConfigInterface<T> {
+    inject?: T;
     directives?: DirectiveInterface[];
     pipes?: PipeInterface[];
     services?: (ServiceInterface | ServiceWithConfigInterface)[];
 }
 
-export function fnComponent(selector: string, component: Function, config: HooksComponentConfigInterface = {}): ComponentInterface {
+export function fnComponent<T = ObjectInterface>(selector: string, component: (injections?: T) => any, config: HooksComponentConfigInterface<T> = {}): ComponentInterface {
 
     Component(selector)(component as any);
     Services(...(config.services || []))(component as any);
     Directives(...(config.directives || []))(component as any);
     Pipes(...(config.pipes || []))(component as any);
-    (component as ComponentInterface).inject = config.inject || {};
+    (component as unknown as ComponentInterface).inject = config.inject || {};
 
     return component as any;
 }
