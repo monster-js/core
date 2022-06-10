afee1415d37673550073ba41bb702e9c59c05570
darius <DariusA.BualanJr@pccw.com>
2022-06-10 16:00:46 +0800

    removed unused codes

--- src/component/utils/auto-resolve-component.ts
+++ src/component/utils/auto-resolve-component.ts
@@ -1,45 +1,5 @@
 import { Container } from "../../dependency-injection/container";
 import { ComponentInterface } from "../../interfaces/component-interface";
-import { ServiceInterface } from "../../interfaces/service.interface";
-import { registerService } from "../../service/utils/register-service";
-import { registerDirectivesToDI } from "./register-directives-to-di";
-import { registerPipesToDI } from "./register-pipes-to-di";
-import { registerServicesToDI } from "./register-services-to-di";
-
-// function registerDirectivesToDI(component: ComponentInterface, di: Container) {
-//     for (const key in component.directives) {
-//         component.directives[key].forEach(directive => di.register(directive, {
-//             target: directive,
-//             instance: null,
-//             singleton: false
-//         }));
-//     }
-// }
-
-// function registerServicesToDI(component: ComponentInterface, di: Container) {
-//     component.services?.forEach(service => {
-//         let target: ServiceInterface;
-//         let config = null;
-//         if (typeof service === 'object') {
-//             target = service.service;
-//             config = service.config;
-//         } else {
-//             target = service;
-//         }
-
-//         registerService(target, di, config)
-//     });
-// }
-
-// function registerPipesToDI(component: ComponentInterface, di: Container) {
-//     for (const key in component.pipes) {
-//         const pipe = component.pipes[key];
-//         di.register(pipe, {
-//             target: pipe,
-//             singleton: false
-//         });
-//     }
-// }
 
 export function autoResolveComponent(component: ComponentInterface) {
 
@@ -49,10 +9,6 @@ export function autoResolveComponent(component: ComponentInterface) {
     const di = new Container(component.dataSource!);
     di.register(component, { target: component, singleton: false, instance: null });
 
-
-    // registerDirectivesToDI(component.directives || {}, di);
-    // registerServicesToDI(component.services || [], di);
-    // registerPipesToDI(component.pipes || {}, di);
 
     return di.resolve(component);
 }
