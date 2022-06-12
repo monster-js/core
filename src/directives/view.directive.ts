d4bf81ef5fcdd90fc1d55db94fbffbc07fb0ec31
darius <mfpjayb@gmail.com>
2022-06-12 22:29:25 +0800

    added v:model support for checkbox and radio buttons

--- src/directives/view.directive.ts
+++ src/directives/view.directive.ts
@@ -1,6 +1,7 @@
 import { detectChanges } from "../component/detect-changes";
 import { DirectiveArgInterface } from "../interfaces/directive-arg.interface";
 import { createWatcher } from "../utils/create-watcher";
+import { errorHandler } from "../utils/error-handler";
 import { watch } from "../utils/watch";
 import { Directive } from "./directive.decorator";
 import { watchDirective } from "./utils/watch-directive";
@@ -48,7 +49,63 @@ export class ViewDirective {
     }
 
     private inputModel(param: DirectiveArgInterface) {
+        const type = param.element.getAttribute('type');
+        if (type && type === 'checkbox') {
+            this.checkboxModel(param);
+            return;
+        } else if (type && type === 'radio') {
+            this.radioModel(param);
+            return;
+        }
         this.processInputAndSelect(param, 'input');
+    }
+
+    private radioModel(param: DirectiveArgInterface) {
+        const valueCaller = param.directive.get;
+        const valueSetter = param.directive.set!;
+        const valueAttribute = param.element.getAttribute('value');
+        if (!valueAttribute) {
+            errorHandler(`Radio buttons must have a value attribute if we want to bind a model to it.`);
+        }
+        param.element.addEventListener('change', (event: any) => {
+            valueSetter(event.target.value)
+            detectChanges(param.component);
+        });
+
+        watchDirective(param, newValue => {
+            this.setRadioChecked(param.element, newValue);
+        });
+        this.setRadioChecked(param.element, valueCaller());
+    }
+
+    private setRadioChecked(element: Element, newValue: any) {
+        const valueAttribute = element.getAttribute('value');
+        if (valueAttribute === newValue) {
+            element.setAttribute('checked', '');
+        } else {
+            element.removeAttribute('checked');
+        }
+    }
+
+    private checkboxModel(param: DirectiveArgInterface) {
+        const valueCaller = param.directive.get;
+        const valueSetter = param.directive.set!;
+        param.element.addEventListener('change', event => {
+            valueSetter((event.target as any).checked);
+            detectChanges(param.component);
+        });
+        watchDirective(param, newValue => {
+            this.setCheckboxChecked(param.element, newValue);
+        });
+        this.setCheckboxChecked(param.element, valueCaller());
+    }
+
+    private setCheckboxChecked(element: Element, value: any) {
+        if (!!value) {
+            element.setAttribute('checked', '');
+        } else {
+            element.removeAttribute('checked');
+        }
     }
 
     private selectModel(param: DirectiveArgInterface) {
